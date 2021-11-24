249a25374440829ea863a61a871b1d24467f3979
chris <Chrs.kaiser3@live.de>
2021-11-24 22:17:19 +0100

    implements grouping and evening of frequencies

--- module/huffman_encoding.py
+++ module/huffman_encoding.py
@@ -6,11 +6,35 @@ class Huffman:
 
     def __init__(self):
         freq = self.map_frequencies(self.EXAMPLE_STRING)
-        sorted_freq = self.sort_frequencies(freq)
+        sorted_freq = self.sort_frequencies(freq) 
+        groups = self.group_frequencies(sorted_freq)
+
+        left_over = None
+        for group in groups.items():
+            # check if there was a leftover from previous cycle
+            if not left_over == None:
+                # if there is a leftover -> attach it at the end of group array
+                group[1].append(left_over)
+
+            # check if group contains odd amount of items
+            if not len(group[1]) % 2 == 0:
+                # if odd -> declare first item a leftover
+                left_over = group[1][0]
+                del group[1][0]
+            print(group)
+                
+    def group_frequencies(self, data: dict) -> dict:
+        groups = {}
+        for char, val in data.items():
+            if not val in groups:
+                groups[val] = [char]
+            else:
+                groups[val].append(char)
+        return groups
 
     def sort_frequencies(self, data: dict) -> dict:
         res = {k: v for k, v in sorted(data.items(), key=lambda item: item[1])}
-        return res 
+        return res
 
     def map_frequencies(self, data: str) -> dict:
         res = {i : data.count(i) for i in set(data)}
