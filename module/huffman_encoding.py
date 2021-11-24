8994c8cb9ff30d9da372fb9aeec6a488a351e1c0
chris <Chrs.kaiser3@live.de>
2021-11-24 20:48:03 +0100

    implements sort_frequencies

--- module/huffman_encoding.py
+++ module/huffman_encoding.py
@@ -1,12 +1,17 @@
+from collections import OrderedDict
+
 class Huffman:
 
     EXAMPLE_STRING: str = "This is the huffman code implementation..."
 
     def __init__(self):
-        frequencies = self.map_frequencies(self.EXAMPLE_STRING)
-        for f, key in frequencies.items():
-            print("{} --> {}".format(f, key))
+        freq = self.map_frequencies(self.EXAMPLE_STRING)
+        sorted_freq = self.sort_frequencies(freq)
 
-    def map_frequencies(self, data):
+    def sort_frequencies(self, data: dict) -> dict:
+        res = {k: v for k, v in sorted(data.items(), key=lambda item: item[1])}
+        return res 
+
+    def map_frequencies(self, data: str) -> dict:
         res = {i : data.count(i) for i in set(data)}
         return res
