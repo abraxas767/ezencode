5e2dc9fee935110e43925da3af19aadc13451933
chris <Chrs.kaiser3@live.de>
2021-11-26 12:50:18 +0100

    implements working grouping of chars

--- module/huffman_encoding.py
+++ module/huffman_encoding.py
@@ -2,36 +2,36 @@ from collections import OrderedDict
 
 class Huffman:
 
-    EXAMPLE_STRING: str = "This is the huffman code implementation..."
+    EXAMPLE_STRING: str = "This üis the huffman code implementation... and i dont know what else to say..."
 
     def __init__(self):
         freq = self.map_frequencies(self.EXAMPLE_STRING)
         sorted_freq = self.sort_frequencies(freq) 
         groups = self.group_frequencies(sorted_freq)
+        self.huffman_tree(groups)
 
+    def huffman_tree(self, data: dict):
         left_over = None
-        for group in groups.items():
+        for group in data.items():
             # check if there was a leftover from previous cycle
-            if not left_over == None:
+            if left_over is not None:
                 # if there is a leftover -> attach it at the end of group array
                 group[1].append(left_over)
-
+                left_over = None
             # check if group contains odd amount of items
-            if not len(group[1]) % 2 == 0:
-                # if odd -> declare first item a leftover
-                left_over = group[1][0]
-                del group[1][0]
-
+            if len(group[1]) % 2 != 0:                 
+                if group[0] == list(data)[-1]:
+                    # append filling leaf node
+                    group[1].append("xx")
+                else:
+                    # if odd -> declare first item a leftover
+                    left_over = group[1][0]
+                    del group[1][0]
             # pair items in groups
-            leaf_node = { group[0] : list(self.chunks(group[1], 2)) }
-            # print(group)
-            self.chain_leaf_nodes(leaf_node)
-            print(leaf_node)
-            print("\n")
+            # leaf_node = { group[0] : list(self.chunks(group[1], 2)) }
+        for i in data.items():
+            print(i)
 
-    def chain_leaf_nodes(self, leaf_node: dict) -> None:
-        pass
-                
     def chunks(self, lst, n):
         """Yield successive n-sized chunks from lst."""
         for i in range(0, len(lst), n):
